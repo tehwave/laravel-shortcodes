ae614b0f92391acd0f4fba225c89a966592191f6
Peter Jørgensen <peterchrjoergensen@gmail.com>
2019-12-19 22:23:24 +0100

    Add methods to retrieve Shortcode classes

--- src/Shortcode.php
+++ src/Shortcode.php
@@ -2,8 +2,24 @@
 
 namespace tehwave\Shortcodes;
 
+use Illuminate\Support\Collection;
+
 class Shortcode
 {
+    /**
+     * The cache of a list of Shortcode classes.
+     *
+     * @var array
+     */
+    protected static $classesCache = [];
+
+    /**
+     * The cache of a list of namespaced Shortcode classes.
+     *
+     * @var array
+     */
+    protected static $namespacedClassesCache = [];
+
     /**
      * The shortcode's attributes.
      *
@@ -37,4 +53,57 @@ class Shortcode
      * @return void
      */
     abstract public function execute();
+
+    /**
+     * Retrieve all of the Shortcode classes.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public static function getClasses(): Collection
+    {
+        if (! isset(static::$classesCache)) {
+            $directory = app()->path('Shortcodes');
+
+            $classes = collect(scandir($directory))
+                ->diff(['..', '.'])
+                ->values();
+
+            static::$classesCache = $classes;
+        }
+
+        return static::$classesCache;
+    }
+
+    /**
+     * Retrieve all of the Shortcode classes in namespace.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public static function getNamespacedClasses(): Collection
+    {
+        if (! isset(static::$namespacedClassesCache)) {
+            $namespacedClasses = static::getClasses()
+                ->transform(function ($class) {
+                    return sprintf(
+                        '%sShortcodes\%s',
+                        app()->getNamespace(),
+                        rtrim($class, '.php')
+                    );
+                });
+
+            static::$namespacedClassesCache = $namespacedClasses;
+        }
+
+        return static::$namespacedClassesCache;
+    }
+
+    /**
+     * A shorthand method for getNamespacedClasses.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public static function all(): Collection
+    {
+        return static::getNamespacedClasses();
+    }
 }
