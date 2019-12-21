31cc2735fc8b4dd466e1fe72282650aa7e7479ce
Peter Jørgensen <peterchrjoergensen@gmail.com>
2019-12-21 22:42:05 +0100

    Create OutputBody.php

---
+++ tests/Shortcodes/OutputBody.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace tehwave\Shortcodes\Tests\Shortcodes;
+
+use tehwave\Shortcodes\Shortcode;
+
+class OutputBody extends Shortcode
+{
+    /**
+     * The code to run when the Shortcode is being compiled.
+     *
+     * You may return a string from here, that will then
+     * be inserted into the content being compiled.
+     *
+     * @return string|null
+     */
+    public function handle(): ?string
+    {
+        return $this->body;
+    }
+}
