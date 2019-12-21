5a08f10f369723b6d16c670bc7ee32d08df33a66
Peter Jørgensen <peterchrjoergensen@gmail.com>
2019-12-21 21:47:24 +0100

    Create TestCase.php

---
+++ tests/TestCase.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace tehwave\Shortcodes\Tests;
+
+use File;
+use Orchestra\Testbench\TestCase as Orchestra;
+use tehwave\Shortcodes\Providers\ShortcodesServiceProvider;
+
+abstract class TestCase extends Orchestra
+{
+   /**
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app): array
+    {
+        return [
+            ShortcodesServiceProvider::class,
+        ];
+    }
+}
