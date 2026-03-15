ab49792c49ec72b093a46e23ffb90e67f47610dc
Peter Jørgensen <4569897+tehwave@users.noreply.github.com>
2026-03-15 16:47:30 +0100

    fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,6 +8,13 @@ use tehwave\Shortcodes\Providers\ShortcodesServiceProvider;
 abstract class TestCase extends Orchestra
 {
     /**
+     * This holds the latest HTTP response from a test.
+     *
+     * @var \Illuminate\Testing\TestResponse|null
+     */
+    protected static $latestResponse;
+
+    /**
      * @param  \Illuminate\Foundation\Application  $app
      */
     protected function getPackageProviders($app): array
